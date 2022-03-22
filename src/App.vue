a07439aad386bb3b2541065ac13d0beb68851c97
Hyeju Helena Hong <56320722+Hyeju1996@users.noreply.github.com>
2022-03-22 15:08:53 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Vue.3!" />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: whitesmoke;
+  margin-top: 60px;
+  background-color: chocolate;
+}
+</style>
